fcd66ff76850c77c1f9e9e934dcda3bf6dc4c839
Ryokuman <ryokuman@naver.com>
2023-04-07 14:12:35 +0900

    feature ( component ) : add size to typo

--- src/component/Typo.js
+++ src/component/Typo.js
@@ -4,6 +4,7 @@ import fonts from "../assets/fonts/fonts";
 const Typo = styled.p`
   ${fonts}
   font-family: InterBold;
+  font-size: ${(props) => props.size || "14px"};
 `;
 
 export default Typo;
