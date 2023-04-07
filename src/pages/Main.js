fd5756940bf8ac0995f9e76085abcb50d220d4d2
Ryokuman <ryokuman@naver.com>
2023-04-07 15:19:23 +0900

    feature ( page ) : add Main with dummy data

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import GraphCard from "../component/GraphCard";
+import Container from "../component/Container";
+
+const dummy = [undefined, { title: "hey" }, { title: "hey" }, { title: "hey" }, { title: "hey" }];
+const Body = styled(Container)`
+  margin-top: 2vh;
+  flex-wrap: wrap;
+`;
+
 function Main() {
-  return <div>main</div>;
+  return (
+    <Body>
+      {dummy.map((v, i) => (
+        <GraphCard key={i} data={v} />
+      ))}
+    </Body>
+  );
 }
 
 export default Main;
