13bb8d6d257574ed294f04262de0a5a28095820e
Ryokuman <ryokuman@naver.com>
2023-04-07 15:18:24 +0900

    feature ( component ) : add CLink and GraphCard

---
+++ src/component/CLink.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const CLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+`;
+
+export default CLink;
