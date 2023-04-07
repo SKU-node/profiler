a0fa15ed7a1c0790fc0dbb0ccfc7894e3bf3f13e
Ryokuman <ryokuman@naver.com>
2023-04-07 12:47:17 +0900

    feature ( package ) : add @nivo/line

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import BarChart from "./component/BarChart";
+import { LineChart } from "./component/LineChart";
+
 function App() {
-  return <div></div>;
+  return (
+    <div style={{ display: "flex" }}>
+      <BarChart />
+      <LineChart />
+    </div>
+  );
 }
 
 export default App;
