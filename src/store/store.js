7d3bc23ebd417d5f111597ac74e027460c218e0e
Ryokuman <ryokuman@naver.com>
2023-05-17 18:05:49 +0900

    fix ( store ) : change obj value

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import graphSlice from "./graphSlice";
 
 const store = configureStore({
-  reducer: { file: graphSlice.reducer },
+  reducer: { graph: graphSlice.reducer },
 });
 
 export default store;
