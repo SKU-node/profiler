801dcd684ad16b01b5aa0adba8a013db025e97a1
Ryokuman <ryokuman@naver.com>
2023-06-13 01:44:18 +0900

    feature ( util ) : add connection for db

---
+++ back/src/util/connect.js
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+import { config } from "dotenv";
+
+// db connecting
+config();
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+  },
+  {
+    define: {
+      freezeTableName: true,
+    },
+  }
+);
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+export default sequelize;
