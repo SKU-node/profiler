33439050d6eea7eae55ec6b00c70f100f61d0cb6
Ryokuman <ryokuman@naver.com>
2023-06-13 06:03:48 +0900

    chore ( store ) : add cookie to userSlice

--- front/src/store/userSlice.js
+++ front/src/store/userSlice.js
@@ -1,13 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getCookie, setCookie, expCookie } from "../utils/cookie";
 
 const userSlice = createSlice({
   name: "user",
   initialState: {
     id: "",
+    uuid: "",
   },
   reducers: {
+    getUser: (state) => {
+      const [id, uuid] = getCookie() || ["", ""];
+      state.id = id;
+      state.uuid = uuid;
+    },
     setUser: (state, action) => {
-      state.id = action.payload;
+      state.id = action.payload.id;
+      state.uuid = action.payload.uuid;
+      setCookie([id, uuid], 24);
+    },
+    deleteUser: (state) => {
+      state.id = "";
+      state.uuid = "";
+      expCookie();
     },
   },
 });
