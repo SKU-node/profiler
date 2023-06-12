ea42bad6a4db84a84d8d8fad404b7056d023b61c
Ryokuman <ryokuman@naver.com>
2023-06-13 05:34:16 +0900

    fix ( backend ) : cors error fix

--- back/src/index.js
+++ back/src/index.js
@@ -8,10 +8,10 @@ import cors from "cors";
 config();
 const app = express();
 const port = process.env.PORT; // backend port number
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 await bulk(); // db bulking
 
-app.use("/", router); // route 연결
+app.use("/api", router); // route 연결
 app.listen(port, () => console.log("Welcome to nodeJS")); // port 설정
