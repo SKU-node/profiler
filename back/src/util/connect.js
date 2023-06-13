7e27b99fde5fcaa5dbac734549b614ca9f8b3824
Ryokuman <ryokuman@naver.com>
2023-06-14 08:49:46 +0900

    by changing db local to RDS add adminer and delete db

--- back/src/util/connect.js
+++ back/src/util/connect.js
@@ -3,21 +3,23 @@ import { config } from "dotenv";
 
 // db connecting
 config();
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  define: {
+    freezeTableName: true,
   },
-  {
-    define: {
-      freezeTableName: true,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 
 try {
   await sequelize.authenticate();
